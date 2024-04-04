a2d164afced1872cd56c42d9a7bb0b91bc610d13
DiegoG0477 <diego.14.13.lopez@gmail.com>
2024-04-04 15:52:00 -0600

    cors configured

--- src/index.js
+++ src/index.js
@@ -1,37 +1,42 @@
 //////////////////////Arranque de aplicación/////////////////////////////
 /////////////////////////////////////////////////////////////////////////
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const { router } = require('./routes/index.js');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const { router } = require("./routes/index.js");
+const cors = require("cors");
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 //////////Variables globales //////////
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 ////////// Inicialización //////////
 const app = express();
 
 ////////// Configuración //////////
-app.set('port', PORT || 4000);
+app.set("port", PORT || 4000);
 
 ////////// Middlewares //////////
-app.use(cors())
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false})); // Para aceptar los formularios de los usuarios.
+app.use(
+    cors({
+        origin: "https://biocare.freemyip.com", // Permitir solicitudes desde este dominio
+        methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+        allowedHeaders: ["Content-Type", "Authorization"], // Encabezados permitidos
+    })
+);
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false })); // Para aceptar los formularios de los usuarios.
 app.use(express.json());
 
 //              ROUTER                 //
-app.use('/', router);
-
+app.use("/", router);
 
 ////////// Public //////////
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 ////////// Start Server //////////
 app.listen(PORT, () => {
-    console.log('Server | PORT', PORT)
+    console.log("Server | PORT", PORT);
 });
